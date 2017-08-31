c8fda51ea193b594a795d891e5729755e97fa143
Connor McTear <xxxcsmct99xxx@yahoo.ca>
2017-08-31 19:01:10 -0400

    Framework

---
+++ ThatZombieSandbox/Assets/Root/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	#region Variables
+	#region Private
+
+
+
+	#endregion
+	#region Public
+
+
+
+	#endregion
+	#endregion
+
+	#region Built-In Methods
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+		RotateView();
+	}	
+	#endregion
+
+	#region Class Specific
+
+	/// <summary>
+	/// Sets the position of the camera
+	/// </summary>
+	private void CameraPosSet(){
+
+	}
+
+	/// <summary>
+	/// Rotates the camera when you hold RMB
+	/// </summary>
+	private void RotateView(){
+		
+		//If holding RMB rotate the camera based on mouse movement.
+
+		if(Input.GetMouseButton(1)){
+
+		}
+	}
+
+	#endregion
+}
---
+++ ThatZombieSandbox/Assets/Root/Scripts/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+	#region Variables
+	#region Private
+
+
+
+	#endregion
+	#region Public
+
+
+
+	#endregion
+	#endregion
+
+	#region Built-In Methods
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}	
+	#endregion
+
+	#region Class Specific
+
+
+	#endregion
+}
